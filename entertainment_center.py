0f7addde41fd8015f9fd639f70cea93f911ba7eb
Steve Hankin <steven.hankin@gmail.com>
2017-06-04 21:52:33 +0100

    Minor improvements for movie data

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,7 +1,8 @@
 import media, fresh_tomatoes
 
 bourne_identity = media.Movie("The Bourne Identity",
-                              "Action spy thriller",
+                              "A man is picked up by a fishing boat, bullet-riddled and suffering from amnesia, "
+                              "before racing to elude assassins and regain his memory",
                               "https://upload.wikimedia.org/wikipedia/en/a/ae/BourneIdentityfilm.jpg",
                               "https://youtu.be/cD-uQreIwEk")
 
@@ -17,7 +18,7 @@ crimson_tide = media.Movie("Crimson Tide",
 
 desperado = media.Movie("Desperado",
                         "A gunslinger is embroiled in a war with a local drug runner",
-                        "https://images-na.ssl-images-amazon.com/images/M/MV5BYjA0NDMyYTgtMDgxOC00NGE0LWJkOTQtNDRjMjEzZmU0ZTQ3XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX182_CR0,0,182,268_AL_.jpg",
+                        "https://images-na.ssl-images-amazon.com/images/M/MV5BYjA0NDMyYTgtMDgxOC00NGE0LWJkOTQtNDRjMjEzZmU0ZTQ3XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX182_CR0,0,182,268_AL_.jpg", # nopep8
                         "https://youtu.be/ESSAlCrKUVg")
 
 # Sort movies by title..
