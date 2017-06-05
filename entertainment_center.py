02f20f7e54adae8b58a42758b1367cacab6896f1
Steve Hankin <steven.hankin@gmail.com>
2017-06-05 16:08:37 +0100

    PEP8 : corrected long lines and trailing whitespaces

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,28 +2,39 @@ import media
 import fresh_tomatoes
 
 bourne_identity = media.Movie("The Bourne Identity",
-                              "A man is picked up by a fishing boat, bullet-riddled and suffering from amnesia, "
-                              "before racing to elude assassins and regain his memory",
-                              "https://upload.wikimedia.org/wikipedia/en/a/ae/BourneIdentityfilm.jpg",
+                              "A man is picked up by a fishing boat, "
+                              "bullet-riddled and suffering from amnesia, "
+                              "before racing to elude assassins and regain"
+                              " his memory",
+                              "https://upload.wikimedia.org/wikipedia/en"
+                              "/a/ae/BourneIdentityfilm.jpg",
                               "https://youtu.be/cD-uQreIwEk")
 
 sexy_beast = media.Movie("Sexy Beast",
-                         "Brutal gangster Don Logan recruits ""retired"" safecracker Gal for one last job",
-                         "https://upload.wikimedia.org/wikipedia/en/9/90/Sexy_beast_ver1.jpg",
+                         "Brutal gangster Don Logan recruits ""retired"" "
+                         "safecracker Gal for one last job",
+                         "https://upload.wikimedia.org/wikipedia/en/9/90/"
+                         "Sexy_beast_ver1.jpg",
                          "https://youtu.be/99ihhUR8lkI")
 
 crimson_tide = media.Movie("Crimson Tide",
                            "Mutiny on a US Nuclear Submarine",
-                           "https://upload.wikimedia.org/wikipedia/en/f/ff/Crimson_tide_movie_poster.jpg",
+                           "https://upload.wikimedia.org/wikipedia/en/f/ff"
+                           "/Crimson_tide_movie_poster.jpg",
                            "https://youtu.be/iS4I2Z1RBIw")
 
 desperado = media.Movie("Desperado",
-                        "A gunslinger is embroiled in a war with a local drug runner",
-                        "https://images-na.ssl-images-amazon.com/images/M/MV5BYjA0NDMyYTgtMDgxOC00NGE0LWJkOTQtNDRjMjEzZmU0ZTQ3XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX182_CR0,0,182,268_AL_.jpg",  # nopep8
+                        "A gunslinger is embroiled in a war with a local"
+                        " drug runner",
+                        "https://images-na.ssl-images-amazon.com/images/M"
+                        "/MV5BYjA0NDMyYTgtMDgxOC00NGE0LWJkOTQtNDRjMjEzZmU"
+                        "0ZTQ3XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX182_CR0"
+                        ",0,182,268_AL_.jpg",
                         "https://youtu.be/ESSAlCrKUVg")
 
 # Sort movies by title..
-movies = sorted([bourne_identity, sexy_beast, crimson_tide, desperado], key=lambda movie: movie.title)
+movies = sorted([bourne_identity, sexy_beast, crimson_tide, desperado],
+                key=lambda movie: movie.title)
 
 # ..and display in default browser
 fresh_tomatoes.Browser(movies).open_movies_page()
--- media.py
+++ media.py
@@ -6,8 +6,10 @@ class Movie:
     Attributes:
         title (str): Movie title
         storyline(str): Film synopsis
-        poster_image_url(str): URL that links to an image of the film poster on the internet
-        trailer_youtube_url(str): URL that links to a trailer for the film on youtube
+        poster_image_url(str): URL that links to an image of the film poster
+                on the internet
+        trailer_youtube_url(str): URL that links to a trailer for the film
+                on youtube
     """
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
@@ -16,6 +18,8 @@ class Movie:
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-    # Launch the default browser and open the YouTube Trailer for this instance's film
     def show_trailer(self):
+        """ Launch the default browser and open the YouTube Trailer
+        for this instance's film
+        """
         webbrowser.open(self.trailer_youtube_url)
