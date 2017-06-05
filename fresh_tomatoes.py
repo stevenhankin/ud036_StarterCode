525abd01e675d94b4f3815c653db8b132409db83
Steve Hankin <steven.hankin@gmail.com>
2017-06-05 12:06:53 +0100

    Refactored to use private variables and methods

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -4,7 +4,7 @@ import re
 
 
 def get_template_data(template_name):
-    """Internal helper function
+    """Retrieves specified template
 
     The templates for the Header, content and tiles are
     stored in HTML "template" files within a resources
@@ -16,36 +16,23 @@ def get_template_data(template_name):
 
 
 class Browser:
-    """A Browser with a Fresh Tomatoes content
-
-        Attributes:
-            movies : array_like(str)
-                     Array of movies
-
-            main_page_head : scalar(str)
-
-            main_page_content : scalar(str)
-
-            movie_tile_content : scalar(str)
+    """A Browser with a Fresh-Tomatoes-Style content
     """
-    movies = []
-    main_page_head = ""
-    main_page_content = ""
-    movie_tile_content = ""
+    _movies = []
+    _main_page_head = ""  # Styles and scripting for the page
+    _main_page_content = ""
+    _movie_tile_content = ""
 
     def __init__(self, movies):
-        self.movies = movies
-        # Styles and scripting for the page
-        self.main_page_head = get_template_data('main_page_head')
-        # The main page layout and title bar
-        self.main_page_content = get_template_data('main_page_content')
-        # A single movie entry html template
-        self.movie_tile_content = get_template_data('movie_tile_content')
+        self._movies = movies
+        self._main_page_head = get_template_data('main_page_head')
+        self._main_page_content = get_template_data('main_page_content')
+        self._movie_tile_content = get_template_data('movie_tile_content')
 
     def __create_movie_tiles_content(self):
         """The HTML content for each film in this section of the page"""
         content = ''
-        for movie in self.movies:
+        for movie in self._movies:
             # Extract the youtube ID from the url
             youtube_id_match = re.search(
                 r'(?<=v=)[^&#]+', movie.trailer_youtube_url)
@@ -55,7 +42,7 @@ class Browser:
                                   else None)
 
             # Append the tile for the movie with its content filled in
-            content += self.movie_tile_content.format(
+            content += self._movie_tile_content.format(
                 movie_title=movie.title,
                 storyline=movie.storyline,
                 poster_image_url=movie.poster_image_url,
@@ -64,16 +51,17 @@ class Browser:
         return content
 
     def open_movies_page(self):
-        """Generates a new HTML File for Fresh Tomatoes contents and opens in the default webbrowser"""
+        """Generates a new HTML File for Fresh Tomatoes contents
+        and opens in the default webbrowser"""
         # Create or overwrite the output file
         output_file = open('fresh_tomatoes.html', 'w')
 
         # Replace the movie tiles placeholder generated content
-        rendered_content = self.main_page_content.format(
+        rendered_content = self._main_page_content.format(
             movie_tiles=self.__create_movie_tiles_content())
 
         # Output the file
-        output_file.write(self.main_page_head + rendered_content)
+        output_file.write(self._main_page_head + rendered_content)
         output_file.close()
 
         # open the output file in the browser (in a new tab, if possible)
